aecac66dce131eb69d842d4f576130b5a388285a
Alex Gusev <alex@flancer64.com>
2018-04-03 10:32:38 +0300

    SUPPLZ-241 Clean up "short" session files

---
+++ etc/bin/root_cron_clean_files.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+## =========================================================================
+#   This script should be launched under root (or those, who has permissions
+#   to change rights on '/var/lib/php/sessions' dir)
+## =========================================================================
+DIR_SESSIONS="/var/lib/php/sessions"
+FILE_LOG="/home/user/fl32botsess.log"
+FILE_REPORT="/home/use/fl32botsess.report"
+GROUP_ORIG="root"
+GROUP_WEB="www-data"
+URL="http://your.shop.com/fl32botsess/clean/files"
+
+# add scan permissions to sessions folder for web-server
+sudo chgrp ${GROUP_WEB} ${DIR_SESSIONS}
+
+# call to cleanup service to be performed under web-server rights
+wget --no-check-certificate --output-file=${FILE_LOG} --output-document=${FILE_REPORT} ${URL}
+
+# restore scan permissions to sessions folder
+sudo chgrp ${GROUP_ORIG} ${DIR_SESSIONS}
